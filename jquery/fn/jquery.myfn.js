5efeb6e3d9d4d2b092f37f4ea87c89473a4c5315
netbuffer <javawiki@163.com>
2016-04-21 16:00:58 +0800

    test

--- jquery/fn/jquery.myfn.js
+++ jquery/fn/jquery.myfn.js
@@ -7,11 +7,12 @@
         console.log("^_^");
         //此处没有必要将this包在$号中如$(this)，因为this已经是一个jQuery对象。
         //$(this)等同于 $($('#element'));
-        this.text("^_^");
-        this.fadeIn('normal', function () {
+        this.css("color","blue").text("^_^").css("border","black 2px solid").css("text-align","center");
+        return this.fadeIn('normal', function () {
             //此处callback函数中this关键字代表一个DOM元素
-            console.log(this);
-            console.log($(this));
+            console.log(this);//dom元素
+            console.log($(this));//jq元素
         });
+        
     };
 })(jQuery);
